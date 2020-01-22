ab7a213f7030517620b08b7fd018c610bdcea89f
riofchrdn <riofchrdn@gmail.com>
2020-01-23 06:45:19 +0700

    membuat function data

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+function get_CURL($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
 
-?>
+  return json_decode($result, true);
+}
